3b522e2e5a7bc3adbdd0f766f69c04250f8a615d
jhermann <jh@web.de>
2013-08-23 00:55:45 +0200

    temporarily added doc tasks to pavement

--- pavement.py
+++ pavement.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=wildcard-import, unused-wildcard-import, invalid-name, unused-import
-""" A library of Paver extensions and generally useful task definitions for
-    any Python project.
+""" A library of Paver extensions and generally useful task definitions.
+
+    cobblestones adds often-used tasks and helper functions on top of Paver,
+    useful for any Python project.
+
+    For more, read the manual at http://jhermann.github.io/cobblestones/.
 
     Copyright © 2013 Jürgen Hermann
+
     Licensed under the Apache License, Version 2.0
 """
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -106,6 +111,87 @@ def init():
     except KeyError:
         pml.writestr("paver/version.py", "VERSION='0.0.0'\n")
     pml.close()
+
+
+#
+# Sphinx Documentation
+#
+# TODO: move to cobblestones.doc.sphinx and cobblestones.doc.gh_pages, and make project layout configurable
+@task
+def doc():
+    """create documentation"""
+    # TODO: Use paver.doctools.html
+    htmldir = projectdir / "build" / "doc" / "html"
+    path("doc/api").rmtree()
+    sh("sphinx-apidoc -f -o %s %s " % (
+        projectdir / "doc" / "api",
+        ' '.join([projectdir / "src" / pkg for pkg in project["packages"]]),
+    ))
+    #path("build/doc/html").rmtree()
+    sh("make -f %s html" % (projectdir / "doc" / "ument"))
+    sh("markdown2 README.md >%s/README.html" % htmldir)
+
+
+@task
+@needs("doc")
+def browse():
+    """create documentation and view in browser"""
+    import webbrowser
+    webbrowser.open("build/doc/html/index.html")
+
+
+@task
+@needs("doc")
+def publish():
+    """create documentation and publish to gh-pages branch"""
+    import shutil
+
+    workdir = projectdir / "build" / "gh-pages"
+    htmldir = projectdir / "build" / "doc" / "html"
+    origin = sh("git config --get remote.origin.url", capture=True).strip()
+
+    workdir.rmtree(); workdir.makedirs()
+    with pushd(workdir):
+        # Specifically checkout gh-pages
+        sh("git init")
+        sh("git remote add -t gh-pages -f origin '%s'" % origin)
+        sh("git checkout gh-pages")
+
+        # Merge updated docs into existing branch
+        for docfile in path(htmldir).walkfiles():
+            # Skip junk files
+            if any(docfile.endswith(i) for i in ("~", ".swp", ".tmp")):
+                continue
+
+            # Copy the updated file and register it for the commit
+            # ("git add" will ignore unchanged files)
+            dstfile = path(docfile.replace(htmldir, '.'))
+            dstfile.parent.exists() or dstfile.parent.makedirs()
+            shutil.copy2(docfile, dstfile)
+            sh("git add '%s'" % dstfile)
+
+        # Push it!
+        sh("git status")
+        sh("git commit -m 'Docs published by paver publish'")
+        sh("git push origin gh-pages")
+        sh("git status")
+
+
+@task
+def publish_init():
+    """create orphaned gh-pages branch"""
+    workdir = projectdir / "build" / "gh-pages"
+    origin = sh("git config --get remote.origin.url", capture=True).strip()
+
+    workdir.rmtree()
+    sh("git clone -q '%s' %s" % (origin, workdir))
+    with pushd(workdir):
+        sh("git checkout --orphan gh-pages")
+        sh("git rm -rf .")
+        path(".nojekyll").write_text("Sphinx generated\n")
+        sh("git add .nojekyll")
+        sh("git commit -a -m 'Initialized by paver publish_init'")
+        sh("git push origin gh-pages")
 
 
 #
