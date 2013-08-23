30de36aa6ef097c57131ea01676ec04e067689d4
jhermann <jh@web.de>
2013-08-23 06:52:51 +0200

    commented

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,12 @@
 #! /bin/bash
 set -e
 set -x
+
+# Bootstrap virtualenv if missing
 venv=1.9.1
 if test ! -f bin/activate; then
     mkdir -p build
-    (cd build \
+    ( cd build \
         && curl -sOkS https://pypi.python.org/packages/source/v/virtualenv/virtualenv-$venv.tar.gz \
         && tar xfz virtualenv-$venv.tar.gz )
     python build/virtualenv-$venv/virtualenv.py --no-site-packages .
@@ -12,6 +14,7 @@ if test ! -f bin/activate; then
     bin/paver init
 fi
 
+# Activate virtualenv and do your thing
 set +x; . bin/activate; set -x
 paver build
 paver doc
