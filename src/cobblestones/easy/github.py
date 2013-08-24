67fa24a95cf7890e8b56bfd5162ffeebfc965f01
jhermann <jh@web.de>
2013-08-24 17:31:04 +0200

    added cobblestones.easy package

---
+++ src/cobblestones/easy/github.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+""" Standard task definitions for projects hosted on GitHub.
+
+    To use these tasks, add this to your pavement::
+    
+        from cobblestones.easy.github import *
+
+    Of course, you can be more selective and use a name list instead of
+    the star import.
+"""
+# Copyright © 2013 Jürgen Hermann
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
